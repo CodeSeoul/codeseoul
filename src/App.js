638a930c08e64bc3bbf309370b64d652b55c97d1
Alex <rollinglex@gmail.com>
2018-07-28 23:45:05 +0900

    setup base style and variable files for styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 
+import baseStyles from './styles/base-styles'
 
 class App extends Component {
   render() {
+    baseStyles()
     return (
       <div className="App">
       </div>
