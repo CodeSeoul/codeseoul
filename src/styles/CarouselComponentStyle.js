b436cf926868adf621acfc0b10c2e66cd2df92cf
Joel Quek <joel.qcx@gmail.com>
2018-08-01 22:54:26 +0900

    styled component for carousel items

--- src/styles/CarouselComponentStyle.js
+++ src/styles/CarouselComponentStyle.js
@@ -4,15 +4,23 @@ const CarouselItem = styled.div`
     width: auto;
     height:auto;
     padding: 50px 0px;
-    text-align:center;
     background: ${props => props.background};
 `
-
 
 const CarouselContainer = styled.div`
     width:80%;
     height: auto;
-    border: 1px solid black;
 `
 
-export {CarouselItem, CarouselContainer};
+const ItemUl = styled.ul`
+    width:auto;
+    height:auto;
+    float: right;
+    padding-right:5vw;
+`
+
+const ItemLi = styled.li`
+    padding: 5px;
+`
+
+export {CarouselItem, CarouselContainer, ItemUl, ItemLi};
