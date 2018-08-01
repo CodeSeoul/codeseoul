c05fb2480004b1986e5d87211548c11b3d8e0e8d
Joel Quek <joel.qcx@gmail.com>
2018-08-01 22:53:32 +0900

    added data and host to carousel item

--- src/components/CarouselComponent.js
+++ src/components/CarouselComponent.js
@@ -2,7 +2,7 @@ import Slider from 'react-slick';
 import React from 'react';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import {CarouselItem, CarouselContainer} from '../styles/CarouselComponentStyle';
+import {CarouselItem, CarouselContainer, ItemUl, ItemLi} from '../styles/CarouselComponentStyle';
 
 const sliderSettings = {
     dots: true,
@@ -15,9 +15,17 @@ const sliderSettings = {
 const CarouselComponent = ({meetupArray}) => {
     const carouselItem = meetupArray.map((obj, i)=>{
        return (<CarouselItem key={i} background={obj.background}>
-                <h3>
-                {obj.title}
-                </h3>
+                <ItemUl>
+                    <ItemLi>
+                        Topic : {obj.title}
+                    </ItemLi>
+                    <ItemLi>
+                        Date : {obj.date}
+                    </ItemLi>
+                    <ItemLi>
+                        Host : {obj.host}
+                    </ItemLi>
+                </ItemUl>
               </CarouselItem>)
     });
 
