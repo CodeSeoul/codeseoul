788140c94f259f435a7ec4a979d80385615b678c
Joel Quek <joel.qcx@gmail.com>
2018-08-01 22:55:03 +0900

    changed colors of background

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,21 +6,21 @@ const placeholderData = [
       'title':'Meetup #1',
       'date' : '2018-08-29 14:00',
       'host' : 'Instructor A',
-      'background': '#2ecc71',
+      'background': '#e1f7d5',
       'link': '#link1'
   },
   {
       'title':'Meetup #2',
       'date' : '2018-08-12 15:00',
       'host' : 'Instructor B',
-      'background': '#1abc9c',
+      'background': '	#ffbdbd',
       'link': '#link2'
   },
   {
       'title':'Meetup #3',
       'date' : '2018-08-18 13:00',
       'host' : 'Instructor C',
-      'background': '#3498db',
+      'background': '#c9c9ff',
       'link': '#link3'
   },
 ];
