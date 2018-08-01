0f7255676c865fd57971d7fc9ff8658259fe7939
Joel Quek <joel.qcx@gmail.com>
2018-08-01 22:17:38 +0900

    added settings for carousel sliders

--- src/components/CarouselComponent.js
+++ src/components/CarouselComponent.js
@@ -4,6 +4,14 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import {CarouselItem, CarouselContainer} from '../styles/CarouselComponentStyle';
 
+const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
 const CarouselComponent = ({meetupArray}) => {
     const carouselItem = meetupArray.map((obj, i)=>{
        return (<CarouselItem key={i} background={obj.background}>
