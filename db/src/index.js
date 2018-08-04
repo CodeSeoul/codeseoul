a420812fb34a35b9d3fdd4c98a7b5b70cef7a1dc
Epikem <dltldls95@naver.com>
2018-08-04 22:00:51 +0900

    added dev helper packages on server - added dotenv - added cross-env - added nodemon - added dev start command on server

--- db/src/index.js
+++ db/src/index.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
+
+const { PORT } = process.env;
+const port = PORT ? PORT : 4000;
 
 app.get('/', (req, res) => {
     res.send('hello world');
 })
 
-app.listen(4000);
+app.listen(port, () => {
+    console.log(`server is running at port ${port}`)
+});
