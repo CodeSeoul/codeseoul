92bb427cb3d39caae26284eeeec9e7d97bbda726
Joel Quek <joel.qcx@gmail.com>
2018-08-08 22:45:09 +0900

    rearranged content

--- src/components/ContentComponent.js
+++ src/components/ContentComponent.js
@@ -6,11 +6,12 @@ const ContentComponent = ({meetupArray}) =>{
 
     return (
         <Row>
-            <Col className={"s12 m12 l6"} > Calendar </Col>
-            <Col className={"s12 m12 l6"} > <Carousel meetupArray={meetupArray}/> </Col>
-            <Col className={"s12 m12 l6"} > FAQ </Col>
-            <Col className={"s12 m12 l6"} > Previous Meetups </Col>
-            <Col className={"s12 m12 l6"} > Resources </Col>
+            <Col className={"s12 m12 l6"}> Calendar </Col>
+            <Col className={"s12 m12 l6"}> <Carousel meetupArray={meetupArray}/> </Col>
+            <Col className={"s12 m12 l6"}> Previous Meetups </Col>
+            <Col className={"s12 m12 l6"}> Resources </Col>
+            <Col className={"s12 m12 l6"}> FAQ </Col>
+            <Col className={"s12 m12 l12"}> Q&A </Col>
         </Row>
     );
 }
