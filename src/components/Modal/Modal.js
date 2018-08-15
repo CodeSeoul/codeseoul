722f05b84fb849306a0dc04714bf638178b22a56
Dasol-Jung <xenosophian@gmail.com>
2018-08-15 09:47:25 +0900

    added a Modal component

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Modal extends React.Component{
+
+    render(){
+        return(
+        <div>
+            Hello World!
+        </div>
+        )
+    }
+}
+
+export default Modal
