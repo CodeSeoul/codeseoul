44a9af6449cd8664c85907252e589c7d3f1bbacf
Epikem <dltldls95@naver.com>
2018-08-17 09:10:24 +0900

    added meetup event fetch api

--- db/src/api/v1.0/event/event.ctrl.js
+++ db/src/api/v1.0/event/event.ctrl.js
@@ -1,4 +1,7 @@
+const axios = require('axios');
 
-exports.MeetupEvents = async (ctx, res) => {
-    
+exports.MeetupEvents = async (req, res) => {
+    const data = await axios.get('https://api.meetup.com/Learn-Teach-Code-Seoul/events?page=1').then(res=>res.data).then(res=>JSON.stringify(res));
+    console.log(data);
+    res.status(200).json({data});
 }
