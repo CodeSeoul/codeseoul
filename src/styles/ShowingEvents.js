cb45730ad69ae2f18bb3800b55e6e2527ad1d91a
Dasol-Jung <xenosophian@gmail.com>
2018-08-18 01:54:22 +0900

    added box-shadow to Events component

--- src/styles/ShowingEvents.js
+++ src/styles/ShowingEvents.js
@@ -20,8 +20,13 @@ const ShowingEventsContainer = styled.div`
 `;
 
 const Events = styled.div`
-
   border: none;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+
+  :hover{
+    box-shadow: 0 6px 15px rgba(0,0,0,0.25), 0 3px 3px rgba(0,0,0,0.22);
+  }
 
   .groupName {
     background-color: ${style.PrimaryColor};
