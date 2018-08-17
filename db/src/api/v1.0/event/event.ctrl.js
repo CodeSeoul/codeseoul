15657dbcdcb532378b0bf1903d91a1d6b746cd39
Epikem <dltldls95@naver.com>
2018-08-17 09:48:24 +0900

    delegated search part of url to meetup api

--- db/src/api/v1.0/event/event.ctrl.js
+++ db/src/api/v1.0/event/event.ctrl.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 exports.MeetupEvents = async (req, res) => {
-    const data = await axios.get('https://api.meetup.com/Learn-Teach-Code-Seoul/events?page=1').then(res=>res.data).then(res=>JSON.stringify(res));
-    console.log(data);
+    // req._parsedUrl.search delegates all querystring params to meetup api
+    const data = await axios.get('https://api.meetup.com/Learn-Teach-Code-Seoul/events' + req._parsedUrl.search).then(res=>res.data).then(res=>JSON.stringify(res));
     res.status(200).json({data});
 }
--- db/test/event.js
+++ db/test/event.js
@@ -9,11 +9,11 @@ describe('event api', () => {
 
     it('/event/meetup/events should return meetup event data', (done) => {
         chai.request(server)
-            .get('/api/v1.0/event/meetup/events')
+            .get('/api/v1.0/event/meetup/events?page=2')
             .end((err, res) => {
                 res.should.have.status(200);
                 expect(res.body).to.be.an('object');
-                expect(JSON.parse(res.body.data)).to.have.lengthOf(1);
+                expect(JSON.parse(res.body.data)).to.have.lengthOf(2);
                 done();
             });
     });
