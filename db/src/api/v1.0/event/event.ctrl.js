bea38bf07a8a892f3b94e6f892823bae282d278a
Epikem <dltldls95@naver.com>
2018-08-17 09:53:35 +0900

    replaced _parsedUrl using parseUrl - referenced https://stackoverflow.com/questions/37285119/what-javascript-library-sets-the-parsedurl-property-on-a-request-object

--- db/src/api/v1.0/event/event.ctrl.js
+++ db/src/api/v1.0/event/event.ctrl.js
@@ -1,7 +1,10 @@
 const axios = require('axios');
+const parseUrl = require('parseUrl');
 
 exports.MeetupEvents = async (req, res) => {
     // req._parsedUrl.search delegates all querystring params to meetup api
-    const data = await axios.get('https://api.meetup.com/Learn-Teach-Code-Seoul/events' + req._parsedUrl.search).then(res=>res.data).then(res=>JSON.stringify(res));
+    const search = parseUrl(req).search;
+    const data = await axios.get('https://api.meetup.com/Learn-Teach-Code-Seoul/events' + search).then(res=>res.data).then(res=>JSON.stringify(res));
+    
     res.status(200).json({data});
 }
