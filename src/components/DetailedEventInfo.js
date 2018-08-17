32709978da81ac1b8bba5054be40e1409c382bf3
Dasol-Jung <xenosophian@gmail.com>
2018-08-17 22:06:15 +0900

    added event description and time to DetailedEventInfo Component

--- src/components/DetailedEventInfo.js
+++ src/components/DetailedEventInfo.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import {CurrentEventsSection} from '../styles/ShowingEvents'
 
 const DetailedEventInfo = (props)=>{
 
+    const {name, description, time, duration } =props.eventInfo
+
     return(
-        <div>{props.eventInfo.name}</div>
+        <React.Fragment>
+        <div>{name}</div>
+        <div>{description}</div>
+        <div>{time} {time+duration}</div>
+        </React.Fragment>
     )
 }
 
