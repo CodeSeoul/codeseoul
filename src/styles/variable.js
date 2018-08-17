588ecae025f55939445d6d3fdddcb8fcee874a08
Dasol-Jung <xenosophian@gmail.com>
2018-08-17 20:04:02 +0900

    updated css variables

--- src/styles/variable.js
+++ src/styles/variable.js
@@ -1,7 +1,15 @@
 
 //sample variables
 //colors
-export const PrimaryColor = '#52dee5';
+export const DarkPrimaryColor = '#9f7e69';
+
+export const DarkSecondaryColor = '#d2bbaO';
+
+export const MiddleColor = '#f2efc7';
+
+export const LightSecondaryColor = 'f7ffeO';
+
+export const LightPrimaryColor = '#ffeee2';
 
 //font
 export const PrimaryFont = 'Roboto, sans-serif';
