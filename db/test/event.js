decc51d5921d7b81cc0ed98cc054aecb6669387d
Epikem <dltldls95@naver.com>
2018-08-17 09:11:09 +0900

    added meetup event api test

---
+++ db/test/event.js
@@ -0,0 +1,20 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+
+let should = chai.should();
+let expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('event api', () => {
+
+    it('/event/meetup/events should return meetup event data', (done) => {
+        chai.request(server)
+            .get('/api/v1.0/event/meetup/events')
+            .end((err, res) => {
+                res.should.have.status(200);
+                expect(res.body).to.be.an('object');
+                expect(JSON.parse(res.body.data)).to.have.lengthOf(1);
+                done();
+            });
+    });
+});
