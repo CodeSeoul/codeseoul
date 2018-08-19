6bd1da013de99b2f94dd4ce414685dc69a8ab3c7
Epikem <dltldls95@naver.com>
2018-08-19 22:41:12 +0900

    added auth check on login page

--- src/pages/AdminLoginPage.js
+++ src/pages/AdminLoginPage.js
@@ -27,10 +27,34 @@ class AdminLoginPage extends React.Component {
 		isAuthenticated: false
 	}
 
+	checkAuth(cb) {
+		getJSON(`/admin/me/info`
+		, {credentials:'include'})
+		.then(res=>{
+			if(res.status >= 200 && res.status < 300){
+				this.setState({isAuthenticated:true});
+			} else {
+				this.setState({isAuthenticated:false});
+			}
+			if (typeof cb === 'function') {
+				cb(true);
+			}
+		})
+		.catch((err) => {
+			console.log('Error fetching authorized user:' + err);
+			this.setState({isAuthenticated:false});
+			if (typeof cb === 'function') {
+				cb(false);
+			}
+		});
+	}
+
 	render = () => {
 		return (
 			<div>
 				<div>Authenticated? : </div><p>{this.state.isAuthenticated.toString()}</p>
+				<button onClick={(e)=>this.checkAuth()}>CheckAuth</button>
+		
 			</div>
 		)
 	}
