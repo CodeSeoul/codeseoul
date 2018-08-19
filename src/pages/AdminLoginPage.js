c3f3f5c93f3427a97a8b4791a370520ce0054675
Epikem <dltldls95@naver.com>
2018-08-20 07:26:07 +0900

    added form to admin page

--- src/pages/AdminLoginPage.js
+++ src/pages/AdminLoginPage.js
@@ -24,12 +24,17 @@ const getJSON = (route, options={}) => {
 
 class AdminLoginPage extends React.Component {
 	state = {
+		formInput:{
+			id: '',
+			pw: ''
+		},
 		isAuthenticated: false
 	}
 
 	register(cb) {
+		const {id, pw} = this.state.formInput;
 		postJSON(`/admin/register/local`
-			, {username:'helow1',password:'asd'})
+			, {username:id,password:pw})
 		.then(res=>{
 			this.setState({isAuthenticated:true});
 			if(typeof cb === 'function')
@@ -41,8 +46,9 @@ class AdminLoginPage extends React.Component {
 	}
 
 	login(cb) {
+		const {id, pw} = this.state.formInput;
 		postJSON(`/admin/login`
-			, {username:'helow1',password:'asd'}
+			, {username:id,password:pw}
 			, {credentials:'include'})
 		.then(res=>{
 			this.setState({isAuthenticated:true});
@@ -92,10 +98,31 @@ class AdminLoginPage extends React.Component {
 		});
 	}
 
+	handleInputChange(e,type){
+		let nextInput = {...this.state.formInput};
+		switch(type){
+			case 'id':
+				nextInput.id = e.target.value;
+				break;
+			case 'pw':
+				nextInput.pw = e.target.value;
+				break;
+			default:
+				throw Error('invalid input type');
+		}
+		this.setState({formInput: nextInput});
+	}
+
 	render = () => {
 		return (
 			<div>
 				<div>Authenticated? : </div><p>{this.state.isAuthenticated.toString()}</p>
+				<form>
+					<input type='text' onChange={(e)=>this.handleInputChange(e,'id')} placeholder='username'/>
+					<input type='password' onChange={(e)=>this.handleInputChange(e,'pw')} placeholder='password'/>
+				</form>
+				<div>{this.state.formInput.id}</div>
+				<div>{this.state.formInput.pw}</div>
 				<button onClick={(e)=>this.register()}>Register</button>
 				<button onClick={(e)=>this.login()}>Login</button>
 				<button onClick={(e)=>this.logout()}>Logout</button>
