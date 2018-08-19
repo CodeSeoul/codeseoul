d9d489926b06f6380d3e95f51bd15daf9a70315c
Epikem <dltldls95@naver.com>
2018-08-19 22:29:23 +0900

    added json util function

--- src/pages/AdminLoginPage.js
+++ src/pages/AdminLoginPage.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
+const postJSON = (route, data, options={}) => {
+	return fetch(`http://localhost:4002/api/v1.0${route}`, {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+      'Content-Type': 'application/json'
+		},
+		...options,
+		body: JSON.stringify(data)
+	});
+}
+
+const getJSON = (route, options={}) => {
+	return fetch(`http://localhost:4002/api/v1.0${route}`, {
+		method: 'GET',
+		headers: {
+			'Accept': 'application/json'
+		},
+		...options
+	});
+}
+
 class AdminLoginPage extends React.Component {
 	state = {
 		isAuthenticated: false
