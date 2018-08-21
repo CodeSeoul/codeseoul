b5a5cec85a4e9f65ee0a454d66b43cae71e95c3f
Epikem <dltldls95@naver.com>
2018-08-21 12:50:37 +0900

    changed LoginPage functions to object member - to pass to LoginForm as a variable

--- src/components/presentational/LoginForm.js
+++ src/components/presentational/LoginForm.js
@@ -1,31 +1,35 @@
 import React from 'react';
 
 const LoginForm = (props) => {
+    const { isAuthenticated, user, formInput } = props.states;
+    const { handleInputChange, register, login, logout, checkAuth } = props.functions;
     return(
-        <div>
+        <section>
             <div>
-                Authenticated User :{' '}
-                {isAuthenticated
-                    ? user.username
-                    : 'not authenticated'}
+                <div>
+                    Authenticated User :{' '}
+                    {isAuthenticated
+                        ? user.username
+                        : 'not authenticated'}
+                </div>
+                <form>                
+                    <input 
+                        type='text' 
+                        onChange={e=>handleInputChange(e, 'id')} 
+                        placeholder='username'/>
+                    <input 
+                        type='password' 
+                        onChange={e=>handleInputChange(e, 'pw')} 
+                        placeholder='password'/>
+                </form>
+                <div>{formInput.id}</div>
+                <div>{formInput.pw}</div>
+                <button onClick={(e)=>register()}>Register</button>
+                <button onClick={(e)=>login()}>Login</button>
+                <button onClick={(e)=>logout()}>Logout</button>
+                <button onClick={(e)=>checkAuth()}>CheckAuth</button>
             </div>
-            <form>
-                <input 
-                    type='text' 
-                    onChange={e=>handleInputChange(e, 'id')} 
-                    placeholder='username'/>
-                <input 
-                    type='password' 
-                    onChange={e=>handleInputChange(e, 'pw')} 
-                    placeholder='password'/>
-            </form>
-            <div>{formInput.id}</div>
-            <div>{formInput.pw}</div>
-            <button onClick={(e)=>register()}>Register</button>
-            <button onClick={(e)=>login()}>Login</button>
-            <button onClick={(e)=>logout()}>Logout</button>
-            <button onClick={(e)=>checkAuth()}>CheckAuth</button>
-        </div>
+        </section>
     )
 }
 
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -48,78 +48,94 @@ class LoginPage extends React.Component {
       });
   }
 
-  login(cb) {
-    const { id, pw } = this.state.formInput;
-    postJSON(
-      `/admin/login`,
-      { username: id, password: pw },
-      { credentials: 'include' }
-    )
-      .then(res => {
-        if (res.status >= 200 && res.status < 300) {
-          this.setState({ isAuthenticated: true });
-          if (typeof cb === 'function') cb(res);
-        }
-      })
-      .catch(err => {
-        console.log('Error fetching authorized user:' + err);
-      });
-  }
-
-  logout(cb) {
-    getJSON(`/admin/logout`, { credentials: 'include' })
-      .then(res => {
-        this.setState({ isAuthenticated: true });
-        if (typeof cb === 'function') cb(true);
-      })
-      .catch(err => {
-        console.log('Error logging out user.');
-        if (typeof cb === 'function') {
-          // user was not logged out
-          cb(true);
-        }
-      });
-  }
-
-  checkAuth(cb) {
-    getJSON(`/admin/me/info`, { credentials: 'include' })
-      .then(res => {
-        if (res.status >= 200 && res.status < 300) {
-          res.json().then(e => {
-            this.setState({ user: e.user });
-          });
-          this.setState({ isAuthenticated: true });
-        } else {
-          this.setState({ isAuthenticated: false });
-        }
-        if (typeof cb === 'function') {
-          cb(true);
-        }
-      })
-      .catch(err => {
-        console.log('Error fetching authorized user:' + err);
-        this.setState({ isAuthenticated: false });
-        if (typeof cb === 'function') {
-          cb(false);
-        }
-      });
-  }
-
-  handleInputChange(e, type) {
-    let nextInput = { ...this.state.formInput };
-    switch (type) {
-      case 'id':
-        nextInput.id = e.target.value;
-        break;
-      case 'pw':
-        nextInput.pw = e.target.value;
-        break;
-      default:
-        throw Error('invalid input type');
-    }
-    this.setState({ formInput: nextInput });
-  }
-
+	functions = {
+		register: (cb) => {
+			const {id, pw} = this.state.formInput;
+			postJSON(`/admin/register/local`
+				, {username:id,password:pw})
+			.then(res=>{
+				this.setState({isAuthenticated:true});
+				if(typeof cb === 'function')
+					cb(res.json().user);
+			})
+			.catch((err) => {
+				console.log('Error registering user:' + err);
+			});
+		},
+	
+		login: (cb) => {
+			const { id, pw } = this.state.formInput;
+			postJSON(
+				`/admin/login`,
+				{ username: id, password: pw },
+				{ credentials: 'include' }
+			)
+				.then(res => {
+					if (res.status >= 200 && res.status < 300) {
+						this.setState({ isAuthenticated: true });
+						if (typeof cb === 'function') cb(res);
+					}
+				})
+				.catch(err => {
+					console.log('Error fetching authorized user:' + err);
+				});
+		},
+	
+		logout: (cb) => {
+			getJSON(`/admin/logout`, { credentials: 'include' })
+				.then(res => {
+					this.setState({ isAuthenticated: true });
+					if (typeof cb === 'function') cb(true);
+				})
+				.catch(err => {
+					console.log('Error logging out user.');
+					if (typeof cb === 'function') {
+						// user was not logged out
+						cb(true);
+					}
+				});
+		},
+		
+		checkAuth: (cb) => {
+			getJSON(`/admin/me/info`, { credentials: 'include' })
+				.then(res => {
+					if (res.status >= 200 && res.status < 300) {
+						res.json().then(e => {
+							this.setState({ user: e.user });
+						});
+						this.setState({ isAuthenticated: true });
+					} else {
+						this.setState({ isAuthenticated: false });
+					}
+					if (typeof cb === 'function') {
+						cb(true);
+					}
+				})
+				.catch(err => {
+					console.log('Error fetching authorized user:' + err);
+					this.setState({ isAuthenticated: false });
+					if (typeof cb === 'function') {
+						cb(false);
+					}
+				});
+		},
+	
+		handleInputChange: (e, type) => {
+			let nextInput = { ...this.state.formInput };
+			switch (type) {
+				case 'id':
+					nextInput.id = e.target.value;
+					break;
+				case 'pw':
+					nextInput.pw = e.target.value;
+					break;
+				default:
+					throw Error('invalid input type');
+			}
+			this.setState({ formInput: nextInput });
+		}
+	}
+	
 	render = () => {
 		return (
 			<LoginForm 
