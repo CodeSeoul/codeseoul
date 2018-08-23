79f0c757fdc15a46c231766db77e38d45dfb5504
Alex <rollinglex@gmail.com>
2018-08-23 20:29:57 +0900

    refactored IsAdmin.js

---
+++ src/components/helper/IsAdmin.js
@@ -0,0 +1,55 @@
+//renders children if user is logged in
+
+import React from 'react';
+
+const getJSON = (route, options = {}) => {
+  return fetch(`http://localhost:4002/api/v1.0${route}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json'
+    },
+    ...options
+  });
+};
+
+class IsAdmin extends React.Component {
+  state = {
+    isAuthenticated: false,
+    user: ''
+  };
+  checkAuth() {
+    getJSON(`/admin/me/info`, { credentials: 'include' })
+      .then(res => {
+        if (res.status >= 200 && res.status < 300) {
+          res.json().then(e => {
+            this.setState({ user: e.user });
+          });
+          this.setState({ isAuthenticated: true });
+        } else {
+          this.setState({ isAuthenticated: false });
+        }
+      })
+      .catch(err => {
+        console.log('Error fetching authorized user:' + err);
+        this.setState({ isAuthenticated: false });
+      });
+  }
+  componentDidMount() {
+    this.checkAuth();
+  }
+  render() {
+    if (!this.state.isAuthenticated) {
+      return null;
+    }
+    const { children } = this.props;
+    const { isAuthenticated, user } = this.state;
+    let childrenWithProps = React.Children.map(children, child => {
+      return React.cloneElement(child, { isAuthenticated, user });
+    });
+    console.log('this.state', this.state);
+    console.log('children', childrenWithProps);
+    return childrenWithProps;
+  }
+}
+
+export default IsAdmin;
