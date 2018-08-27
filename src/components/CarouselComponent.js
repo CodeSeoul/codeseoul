53aa474155253b864f33c65855eada19a6b7854d
Epikem <dltldls95@naver.com>
2018-08-27 19:51:53 +0900

    add ContentClipper to carousel

---
+++ src/components/CarouselComponent.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {CarouselItem, CarouselContainer, ContentClipper, ItemUl, ItemLi} from '../styles/CarouselComponentStyle';
+
+const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+};
+
+class CarouselComponent extends React.Component {
+    render = () => {
+        const carouselItem = this.props.meetupArray.map((obj, i)=>{
+           return (<CarouselItem key={i} background={obj.background}>
+            <ItemUl>
+                <ItemLi>
+                    Topic : {obj.title}
+                </ItemLi>
+                <ItemLi>
+                    Date : {obj.date}
+                </ItemLi>
+                <ItemLi>
+                    Host : {obj.host}
+                </ItemLi>
+            </ItemUl>
+            </CarouselItem>)
+        });
+    
+        return (
+            <ContentClipper>
+                <CarouselContainer>
+                    {carouselItem}
+                </CarouselContainer>
+            </ContentClipper>
+        );
+    }
+}
+
+export {CarouselComponent};
+
