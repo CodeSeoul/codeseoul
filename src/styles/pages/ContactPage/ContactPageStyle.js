e5fa84d15a657d292eb75899e379d72b0511d3bd
Dasol-Jung <xenosophian@gmail.com>
2018-08-27 23:57:32 +0900

    built a basic structure of contact page

---
+++ src/styles/pages/ContactPage/ContactPageStyle.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import * as style from "../../variable";
+
+const ContactPageStyle = styled.div`
+
+  header{
+      font-size : 3rem;
+      min-height : 200px;
+      display : flex;
+      flex-direction : column;
+      justify-content :center;
+      align-items : center;
+  }
+
+`
+
+export default ContactPageStyle;
