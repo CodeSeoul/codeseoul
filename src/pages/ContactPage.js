9fc394328056e3014301bc799db382e6bce7da04
Dasol-Jung <xenosophian@gmail.com>
2018-08-27 23:57:32 +0900

    built a basic structure of contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React from "react";
+import ContactPageStyle from "../styles/pages/ContactPage/ContactPageStyle";
 
-const ContactPage = () => (
-  <div>Contact page</div>
-);
+const ContactPage = () => {
+  return (
+    <ContactPageStyle>
+        <header>
+          <h1>Contact Us</h1>
+        </header>
+
+        <div className="primary-contact">
+          <div id="email">Email : wcoding@wcoding.org</div>
+          <div id="phone">Phone : 02-1232-1124</div>
+        </div>
+
+        <div className="social-media">
+          <ul>
+            <li>Facebook</li>
+            <li>Meetup</li>
+            <li>Tweeter</li>
+            <li>Kakao</li>
+          </ul>
+        </div>
+
+        <div className="form">
+          <form action="">
+            <input type="text" />
+            <input type="email" />
+            <input type="textarea" />
+            <button id="submit">Send</button>
+          </form>
+        </div>
+    </ContactPageStyle>
+  );
+};
 
 export default ContactPage;
