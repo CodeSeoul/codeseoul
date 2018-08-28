1be812d8a4e920d13c62e2403fd3b3e522c2231b
Dasol-Jung <xenosophian@gmail.com>
2018-08-28 20:31:56 +0900

    created contact page form event handler

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,42 +1,60 @@
 import React from "react";
 import ContactPageStyle from "../styles/pages/ContactPage/ContactPageStyle";
 
-const ContactPage = () => {
-  return (
-    <ContactPageStyle>
+class ContactPage extends React.Component {
+
+  state={
+    'first-name' : null,
+    'last-name' : null,
+    'email' : null,
+    'message' : null
+  }
+  changeHandler = event => {
+    const target = event.target;
+    const value = target.value;
+    const id = target.id;
+    
+    this.setState({
+      [id] : value
+    })
+    console.log(this.state);
+  };
+  render() {
+    return (
+      <ContactPageStyle>
         <header>
           <h1>Contact Us</h1>
         </header>
         <div className="container">
-        <div className="primary-contact">
-          <div id="email">Email : wcoding@wcoding.org</div>
-          <div id="phone">Phone : 02-1232-1124</div>
-        </div>
-
-        <div className="social-media">
-          <ul>
-            <li>Facebook</li>
-            <li>Meetup</li>
-            <li>Tweeter</li>
-            <li>Kakao</li>
-          </ul>
-        </div>
-
-          <form action="">
-            <label htmlFor='first-name'>First Name</label>
-            <input type="text" id='first-name' />
-            <label htmlFor="last-name">Last Name</label>
-            <input type="text" id='last-name' />
-            <label htmlFor="email">Email</label>
-            <input type="email" id='email' />
-            <label htmlFor="message">Message</label>
-            <input type="textarea" id='message' />
-            <button id="submit">Send</button>
-          </form>
+          <div className="primary-contact">
+            <div id="email">Email : wcoding@wcoding.org</div>
+            <div id="phone">Phone : 02-1232-1124</div>
           </div>
 
-    </ContactPageStyle>
-  );
-};
+          <div className="social-media">
+            <ul>
+              <li>Facebook</li>
+              <li>Meetup</li>
+              <li>Tweeter</li>
+              <li>Kakao</li>
+            </ul>
+          </div>
+
+          <form action="">
+            <label htmlFor="first-name">First Name</label>
+            <input type="text" id="first-name" onChange={this.changeHandler} />
+            <label htmlFor="last-name">Last Name</label>
+            <input type="text" id="last-name" onChange={this.changeHandler} />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" onChange={this.changeHandler} />
+            <label htmlFor="message">Message</label>
+            <input type="textarea" id="message" onChange={this.changeHandler} />
+            <button id="submit">Send</button>
+          </form>
+        </div>
+      </ContactPageStyle>
+    );
+  }
+}
 
 export default ContactPage;
