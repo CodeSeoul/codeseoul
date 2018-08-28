c9180dce0757e9becc14f4ca8ab95a9f860d3122
Alex <rollinglex@gmail.com>
2018-08-29 02:32:58 +0900

    setup Footer and style

---
+++ src/components/containers/nav_footer/FooterContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Footer from '../../presentational/Nav_Footer/Footer';
+
+class FooterContainer extends React.Component {
+  render() {
+    return <Footer />;
+  }
+}
+
+export default FooterContainer;
