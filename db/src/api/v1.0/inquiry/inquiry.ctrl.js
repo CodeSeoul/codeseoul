2be29d4bc491c325cd4d9564bdd71ab471aac1f4
Dasol-Jung <xenosophian@gmail.com>
2018-08-29 23:39:17 +0900

    added inquiry post request function

--- db/src/api/v1.0/inquiry/inquiry.ctrl.js
+++ db/src/api/v1.0/inquiry/inquiry.ctrl.js
@@ -1,5 +1,6 @@
-const axios = require('axios');
+const axios = require("axios");
+const qs = require("qs");
 
-exports.PostInquiries = async(req, res) => {
-    res.send(req.body);
-}
+exports.PostInquiries = async (req, res) => {
+  res.send(req.body);
+};
