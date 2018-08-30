a18ec2d6151d389d8318572147befcbfcf8898e7
Dasol-Jung <xenosophian@gmail.com>
2018-08-30 21:55:26 +0900

    added inquiry validator

--- db/src/models/Inquiry.js
+++ db/src/models/Inquiry.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const InquirySchema = new Schema({
-    firstName : String,
-    lastName : String,
-    email: String,
-    message : String 
+    firstName : {type : String, required : true},
+    lastName : {type : String, required : true},
+    email: {type : String, required : true, match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']},
+    message : {type : String, required : true}
 });
 
 module.exports = Inquiry = mongoose.model('Inquiry', InquirySchema);
