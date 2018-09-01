89699db1acf7be3335e3d4100b4b78f91675a7d5
Alex <rollinglex@gmail.com>
2018-09-01 22:44:23 +0900

    restyled Events page header

--- src/styles/pages/eventsPage/header.js
+++ src/styles/pages/eventsPage/header.js
@@ -1,14 +1,15 @@
-import styled from 'styled-components';
-import * as style from '../../variable';
+import styled from "styled-components";
+import * as style from "../../variable";
 
 let Header = styled.header`
-  text-align: center;
-  margin: 1rem auto;
-  font-size: 9vmin;
-  background-color: ${style.ThirdColor};
-  height: 10vmin;
+  font-size: 7vmin;
+  background-color: #f77f00;
+  height: 12vmin;
   color: white;
-  font-family: ${style.PrimaryFont};
+  font-family: Roboto, sans-serif;
+  -webkit-clip-path: polygon(0 0, 0 100%, 100% 25%, 100% 0%);
+  clip-path: polygon(0 0, 0 100%, 100% 25%, 100% 0%);
+  padding: 0.5rem 0 0 3rem;
 `;
 
 export default Header;
