baf1014c9d6729a39ceeaeedd88a234b618d8311
Alex <rollinglex@gmail.com>
2018-09-03 03:02:34 +0900

    Give access to font-awesome to all components via font-awsome library

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import baseStyles from '../styles/base-styles';
-import { BrowserRouter, Route } from 'react-router-dom';
-import NavMenuContainer from './containers/nav_footer/NavMenuContainer';
-import FooterContainer from './containers/nav_footer/FooterContainer';
-import HomePage from '../pages/HomePage';
-import AboutPage from '../pages/AboutPage';
-import ContactPage from '../pages/ContactPage';
-import EventsPage from '../pages/EventsPage';
-import LoginPage from '../pages/LoginPage';
+import React from "react";
+import baseStyles from "../styles/base-styles";
+import { BrowserRouter, Route } from "react-router-dom";
+import NavMenuContainer from "./containers/nav_footer/NavMenuContainer";
+import FooterContainer from "./containers/nav_footer/FooterContainer";
+import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
+import ContactPage from "../pages/ContactPage";
+import EventsPage from "../pages/EventsPage";
+import LoginPage from "../pages/LoginPage";
+
+//add font-awesome access to all components.
+//docs: https://fontawesome.com/how-to-use/on-the-web/using-with/react
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStroopwafel } from "@fortawesome/free-solid-svg-icons";
+library.add(faStroopwafel);
 
 const PrimaryLayout = () => (
   <div className="primary-layout">
