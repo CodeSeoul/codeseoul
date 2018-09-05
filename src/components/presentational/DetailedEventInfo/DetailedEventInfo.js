2466befd8b046c4a4e5ca1dfa3ad84ee601243a4
Dasol-Jung <xenosophian@gmail.com>
2018-09-05 22:57:37 +0900

    DetailedEventInfo prop-type check

--- src/components/presentational/DetailedEventInfo/DetailedEventInfo.js
+++ src/components/presentational/DetailedEventInfo/DetailedEventInfo.js
@@ -2,7 +2,7 @@ import React from "react";
 import { DetailedEventInfoStyle } from "../../../styles/pages/EventsPage/ShowingEvents";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser} from '@fortawesome/free-solid-svg-icons'
-
+import PropTypes from 'prop-types';
 
 const DetailedEventInfo = props => {
   const { name, description, time, local_time, duration } = props.eventInfo;
@@ -44,5 +44,15 @@ const DetailedEventInfo = props => {
     </DetailedEventInfoStyle>
   );
 };
+
+DetailedEventInfo.propTypes ={
+  eventInfo : PropTypes.shape({
+    name : PropTypes.string.isRequired,
+    description : PropTypes.string.isRequired,
+    time  : PropTypes.number.isRequired,
+    local_time : PropTypes.string.isRequired,
+    duration : PropTypes.number.isRequired
+  })
+}
 
 export default DetailedEventInfo;
