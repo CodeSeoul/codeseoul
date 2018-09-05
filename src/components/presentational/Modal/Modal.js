d8bb4803e0c9d35ce8237e3326bf9478b2f3e70c
Dasol-Jung <xenosophian@gmail.com>
2018-09-05 22:45:04 +0900

    modal prop-type check

--- src/components/presentational/Modal/Modal.js
+++ src/components/presentational/Modal/Modal.js
@@ -6,6 +6,7 @@ import {
 } from "../../../styles/helpers/ModalStyle"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import PropTypes from 'prop-types';
 
 class Modal extends React.Component {
   render() {
@@ -27,6 +28,10 @@ class Modal extends React.Component {
       );
     }
   }
+}
+
+Modal.propTypes = {
+  show : PropTypes.bool.isRequired
 }
 
 export default Modal;
